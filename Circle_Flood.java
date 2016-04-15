155b33308f49e956ec4299d2fa8e54d13695c531
Beto Luna <beto@Betos-iMac.local>
2016-04-15 17:27:14 -0400

    finish circle flood

--- Circle_Flood.java
+++ Circle_Flood.java
@@ -11,7 +11,7 @@ import ij.gui.*;
  * Date: April 13, 2016
  * Overview Description of Plugin:
  * perform 2 types of flood fill algorithms to fill in each circle found in the image. 
- * Metohds used: recursive, and coherence.
+ * Methods used: recursive, and coherence. Please set the boolean 'useCoherence' accordingly.
  */
 
 /**
@@ -25,11 +25,11 @@ public class Circle_Flood implements PlugInFilter {
     ArrayList<Circle> circleList;
 
 	//determine whether to use coherence or recursion floodFill
-    private boolean useCoherence = true;
+    private boolean useCoherence = false;
     
-    private int numCircles;// num of circles to find   
-    private int minRad;
-    private int maxRad;
+    private int numCircles = 10;// num of circles to find   
+    private int minRad = 25;
+    private int maxRad = 30;
     
 	/**
 	 * This method gets called by ImageJ / Fiji to determine
@@ -120,126 +120,127 @@ public class Circle_Flood implements PlugInFilter {
         int height = ip2.getHeight();
         byte byteArrayImage[] = (byte[]) ip2.getPixels();//array of image pixels
 
-        if (readParameters()) {
-
-            //create a look up table 
-            //increment theta each time by some proportion of 360 degrees
-            int thetaIncrement = Math.round(minRad * 45);
-            int step = 2;//radius increment  
-            int radiiSpan = ((maxRad - minRad) / step) + 1;
-            int lookUpTable[][][] = new int[2][thetaIncrement][radiiSpan];
-            int tableLen = 0;
-            for (int r = minRad; r <= maxRad; r = r + step) {
-                tableLen = 0;
-                for (int numerator = 0; numerator < thetaIncrement; numerator++) {
-                    double theta = (2 * Math.PI * (double) numerator) / (double) thetaIncrement;
-                    int radiusIndex = (r - minRad) / step;
-                    int rCosTheta = (int) Math.round((double) r * Math.cos(theta));
-                    int rSinTheta = (int) Math.round((double) r * Math.sin(theta));
-                    if ((rCosTheta != lookUpTable[0][tableLen][radiusIndex]) && (rSinTheta != lookUpTable[1][tableLen][radiusIndex]) || (tableLen == 0)) {
-                        lookUpTable[0][tableLen][radiusIndex] = rCosTheta;
-                        lookUpTable[1][tableLen][radiusIndex] = rSinTheta;
-                        tableLen++;
-                    }
+        //create a look up table 
+        //increment theta each time by some proportion of 360 degrees
+        int thetaIncrement = Math.round(minRad * 45);
+        int step = 2;//radius increment  
+        int radiiSpan = ((maxRad - minRad) / step) + 1;
+        int lookUpTable[][][] = new int[2][thetaIncrement][radiiSpan];
+        int tableLen = 0;
+        for (int r = minRad; r <= maxRad; r = r + step) {
+            tableLen = 0;
+            for (int numerator = 0; numerator < thetaIncrement; numerator++) {
+                double theta = (2 * Math.PI * (double) numerator) / (double) thetaIncrement;
+                int radiusIndex = (r - minRad) / step;
+                int rCosTheta = (int) Math.round((double) r * Math.cos(theta));
+                int rSinTheta = (int) Math.round((double) r * Math.sin(theta));
+                if ((rCosTheta != lookUpTable[0][tableLen][radiusIndex]) && (rSinTheta != lookUpTable[1][tableLen][radiusIndex]) || (tableLen == 0)) {
+                    lookUpTable[0][tableLen][radiusIndex] = rCosTheta;
+                    lookUpTable[1][tableLen][radiusIndex] = rSinTheta;
+                    tableLen++;
                 }
             }
-
-            //create and fill Hough Accumulator 
-            double ACC[][][] = new double[width][height][radiiSpan];
-            for (int y = 1; y < height - 1; y++) {
-                for (int x = 1; x < width - 1; x++) {
-                    for (int r = minRad; r <= maxRad; r = r + step) {
-                        //if potential edge pixel
-                        if (ip2.get(x, y) == 255) {
-                            int radiusIndex = (r - minRad) / step;
-                            for (int i = 0; i < tableLen; i++) {
-                                int a = x + lookUpTable[1][i][radiusIndex];
-                                int b = y + lookUpTable[0][i][radiusIndex];
-                                if ((b >= 0) && (b < height) && (a >= 0) && (a < width)) {
-                                    ACC[a][b][radiusIndex] += 1;//increase accumulator at this index
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            ImageProcessor edgeImProc = new ByteProcessor(width, height);
-            byte[] edges = (byte[]) edgeImProc.getPixels();
-
-            // compute circle origin coordiantes and radius
-            int maxX = 0, maxY = 0, maxR = 0;
-            for (int i = 0; i < numCircles; i++) {
-                double counterMax = -1;
-                for (int r = minRad; r <= maxRad; r = r + step) {
-                    int radiusIndex = (r - minRad) / step;
-                    for (int y = 0; y < height; y++) {
-                        for (int x = 0; x < width; x++) {
-                            if (ACC[x][y][radiusIndex] > counterMax) {
-                                counterMax = ACC[x][y][radiusIndex];
-                                maxX = x;
-                                maxY = y;
-                                maxR = r;
-                            }
-                        }
-                    }
-                }
-                circleList.add(new Circle(maxX, maxY, maxR));// add this circle
-
-                //remove unwanted neighbor values (less than maxima) from the accumulator space
-                //from center maxima to a distance of approx maximum radius divided by 2  
-                double rOver2 = maxR / 2.0;
-                int x1, x2, y1, y2;            
-                if ((x1 = (int) Math.floor((double) maxX - rOver2)) < 0)
-                    x1 = 0;
-                if ((x2 = (int) Math.ceil((double) maxX + rOver2) + 1) > width)
-                    x2 = width;
-                if ((y1 = (int) Math.floor((double) maxY - rOver2)) < 0)
-                    y1 = 0;
-                if ((y2 = (int) Math.ceil((double) maxY + rOver2) + 1) > height)
-                    y2 = height;
-
-                double rOver2Squared = rOver2 * rOver2;
-                for (int r = minRad; r <= maxRad; r = r + step) {
-                    int radiusIndex = (r - minRad) / step;
-                    for (int ii = y1; ii < y2; ii++) {
-                        for (int j = x1; j < x2; j++) {
-                            if (rOver2Squared > (j - maxX) * (j - maxX) + (ii - maxY) * (ii - maxY)) {
-                                ACC[j][ii][radiusIndex] = 0;//replace value with zero
-                            }
-                        }
-                    }
-                }
-            }
-
-            // draw edges in image with intensity value 127 (max positive value for a byte in two's complement)
-            int edgePos = 0;
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-                    if (byteArrayImage[x + width * y] != 0) {
-                        edges[edgePos] = 127; 
-                    } else {
-                        edges[edgePos] = 0;
-                    }
-
-                    edgePos++;
-                }
-            }
-
-            for (Circle circle : circleList) {
-                int x = circle.getX();
-                int y = circle.getY();
-                int r = circle.getRadius();
-
-                if (useCoherence) {
-                    coherenceFloodFill(x, y, r, edgeImProc);
-                } else {
-                    recursiveFloodFill(edgeImProc, x, y, r, x, y, 255);
-                }     
-            }
-
-            new ImagePlus("Circular_Hough", edgeImProc).show();
         }
+
+        //create and fill Hough Accumulator 
+        double ACC[][][] = new double[width][height][radiiSpan];
+        for (int y = 1; y < height - 1; y++) {
+            for (int x = 1; x < width - 1; x++) {
+                for (int r = minRad; r <= maxRad; r = r + step) {
+                    //if potential edge pixel
+                    if (ip2.get(x, y) == 255) {
+                        int radiusIndex = (r - minRad) / step;
+                        for (int i = 0; i < tableLen; i++) {
+                            int a = x + lookUpTable[1][i][radiusIndex];
+                            int b = y + lookUpTable[0][i][radiusIndex];
+                            if ((b >= 0) && (b < height) && (a >= 0) && (a < width)) {
+                                ACC[a][b][radiusIndex] += 1;//increase accumulator at this index
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        ImageProcessor edgeImProc = new ByteProcessor(width, height);
+        byte[] edges = (byte[]) edgeImProc.getPixels();
+
+        // compute circle origin coordiantes and radius
+        int maxX = 0, maxY = 0, maxR = 0;
+        for (int i = 0; i < numCircles; i++) {
+            double counterMax = -1;
+            for (int r = minRad; r <= maxRad; r = r + step) {
+                int radiusIndex = (r - minRad) / step;
+                for (int y = 0; y < height; y++) {
+                    for (int x = 0; x < width; x++) {
+                        if (ACC[x][y][radiusIndex] > counterMax) {
+                            counterMax = ACC[x][y][radiusIndex];
+                            maxX = x;
+                            maxY = y;
+                            maxR = r;
+                        }
+                    }
+                }
+            }
+            circleList.add(new Circle(maxX, maxY, maxR));// add this circle
+
+            //remove unwanted neighbor values (less than maxima) from the accumulator space
+            //from center maxima to a distance of approx maximum radius divided by 2  
+            double rOver2 = maxR / 2.0;
+            int x1, x2, y1, y2;            
+            if ((x1 = (int) Math.floor((double) maxX - rOver2)) < 0)
+                x1 = 0;
+            if ((x2 = (int) Math.ceil((double) maxX + rOver2) + 1) > width)
+                x2 = width;
+            if ((y1 = (int) Math.floor((double) maxY - rOver2)) < 0)
+                y1 = 0;
+            if ((y2 = (int) Math.ceil((double) maxY + rOver2) + 1) > height)
+                y2 = height;
+
+            double rOver2Squared = rOver2 * rOver2;
+            for (int r = minRad; r <= maxRad; r = r + step) {
+                int radiusIndex = (r - minRad) / step;
+                for (int ii = y1; ii < y2; ii++) {
+                    for (int j = x1; j < x2; j++) {
+                        if (rOver2Squared > (j - maxX) * (j - maxX) + (ii - maxY) * (ii - maxY)) {
+                            ACC[j][ii][radiusIndex] = 0;//replace value with zero
+                        }
+                    }
+                }
+            }
+        }
+
+        // draw edges in image with intensity value 127 (max positive value for a byte in two's complement)
+        int edgePos = 0;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (byteArrayImage[x + width * y] != 0) {
+                    edges[edgePos] = 127; 
+                } else {
+                    edges[edgePos] = 0;
+                }
+
+                edgePos++;
+            }
+        }
+
+        String using = "";
+        for (Circle circle : circleList) {
+            int x = circle.getX();
+            int y = circle.getY();
+            int r = circle.getRadius();
+
+            if (useCoherence) {
+                coherenceFloodFill(x, y, r, edgeImProc);
+                using = "coherence";
+            } else {
+                recursiveFloodFill(edgeImProc, x, y, r, x, y, 255);
+                using = "recursion";
+            }     
+        }
+
+        new ImagePlus("Circular_Hough " + using, edgeImProc).show();
+
     }//end run
 
     /**
@@ -254,7 +255,7 @@ public class Circle_Flood implements PlugInFilter {
         //compute if u and v are within circle boundaries by the distance formula 
         int d = (int) Math.sqrt( (double) ((u - CX) * (u - CX) + (v - CY) * (v - CY)) );
 
-        if (d > R) return;       
+        if (proc.getPixel(u, v) == 255 || d > R) return;       
        
         proc.set(u, v, label);
 
@@ -301,25 +302,5 @@ public class Circle_Flood implements PlugInFilter {
             }
         }          
     }
-
-    boolean readParameters() {
-        GenericDialog gd = new GenericDialog("Hough Parameters", IJ.getInstance());
-        gd.addNumericField("Minimum radius (in pixels) :", 10, 0);
-        gd.addNumericField("Maximum radius (in pixels)", 20, 0);
-        gd.addNumericField("Number of Circles (NC): (enter 0 if using threshold)", 10, 0);
-
-        gd.showDialog();
-
-        if (gd.wasCanceled()) {
-            return (false);
-        }
-
-        minRad = (int) gd.getNextNumber();
-        maxRad = (int) gd.getNextNumber();
-        numCircles = (int) gd.getNextNumber();
-
-        return true;
-    }
-
     
 }
